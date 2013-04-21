4cba5042dcfdb0b086cc82e5563969a9eb214870
Wu Jing <wumailjing@gmail.com>
2013-04-21 23:27:06 +0800

    add models

--- feedin/models.py
+++ feedin/models.py
@@ -1,5 +1,30 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 
 # Create your models here.
 
+class Tag(models.Model):
+	tag = models.CharField(max_length=100)
+
+	def __unicode__(self):
+		return self.tag
+
+class Commodity(models.Model):
+	# 商品标题
+	title = models.CharField(max_length=255)
+	# 原价
+	price = models.DecimalField(max_digits=12, decimal_places=2)
+	# 促销价
+	discount_price = models.DecimalField(max_digits=12, decimal_places=2)
+	# 图片地址 
+	imgurl = models.CharField(max_length=255)
+	# 三十天销售量
+	seles = models.IntegerField(default=0)
+	# 评价星级
+	appraisal = models.IntegerField(default=0)
+
+	tags = models.ManyToManyField(Tag)
+
+	def __unicode__(self):
+		return self.title
 
